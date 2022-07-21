63dbc996ab3377fb5a32b42e6025fe2139bca0e7
vikram <bhamrev@stpaulsschool.org.uk>
2022-07-21 11:23:10 -0700

    chenged path

--- tests/test_Examples/test_JWST.py
+++ tests/test_Examples/test_JWST.py
@@ -5,6 +5,7 @@ import astropy.io.fits as pyfits
 
 def test_retrieve_psf():
     module_path = os.path.dirname(psfr.__file__)
-    psf_filename = module_path + '\Data\JWST_mock\psf_f090w_supersample5_crop.fits'
-    # kernel = pyfits.get_data(psf_filename)
+    psf_filename = module_path + '/Data/JWST_mock/psf_f090w_supersample5_crop.fits'
     npt.assert_equal(os.path.isfile(psf_filename),True, err_msg = 'Psf file not found')
+
+test_retrieve_psf()
